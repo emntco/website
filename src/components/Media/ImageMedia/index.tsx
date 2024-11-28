787f0045f83b3f0ba7119066b10e03c6c163d0e4
Zayadur <zayadur@me.com>
2024-11-28 14:15:36 -0500

    Revert normalized Media picture height, update Card headings
    Height needs to be applied more specifically to metaImg

--- src/components/Media/ImageMedia/index.tsx
+++ src/components/Media/ImageMedia/index.tsx
@@ -63,7 +63,7 @@ export const ImageMedia: React.FC<MediaProps> = (props) => {
     <picture>
       <NextImage
         alt={alt || ''}
-        className={cn(imgClassName, 'object-cover h-56')}
+        className={cn(imgClassName, 'object-cover')}
         fill={fill}
         height={!fill ? height : undefined}
         placeholder="blur"
