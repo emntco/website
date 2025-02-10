43b8bdace337058aecabc5c2fdf265d2d8a740a9
Zayadur <zayadur@me.com>
2025-02-09 23:48:15 -0500

    Add Sentry monitoring

--- next.config.js
+++ next.config.js
@@ -1,27 +1,45 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from '@payloadcms/next/withPayload';
+import { withSentryConfig } from '@sentry/nextjs';
+import redirects from './redirects.js';
 
-import redirects from './redirects.js'
+const serverUrl =
+  process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.NEXT_PUBLIC_SERVER_URL ?? 'http://localhost:3000';
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const remotePatterns = serverUrl
+  ? (() => {
+    const { hostname, protocol } = new URL(serverUrl);
+    return [
+      {
+        hostname,
+        protocol: protocol.replace(':', ''),
+      },
+    ];
+  })()
+  : [];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
+    remotePatterns,
   },
   reactStrictMode: true,
   redirects,
-}
+};
 
-export default withPayload(nextConfig)
+const configWithPayload = withPayload(nextConfig);
+
+export default withSentryConfig(configWithPayload, {
+  org: 'emnt',
+  project: 'emnt',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  tunnelRoute: '/monitoring',
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
